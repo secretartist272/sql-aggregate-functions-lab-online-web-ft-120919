1c092096f1b8418dc6f6b5e9588dd3996450d7b2
Jacqueline Freeland <secretartist272@gmail.com>
2020-01-24 20:51:16 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,17 +5,17 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
-  "SELECT MIN(gpa) as lowest_gpa FROM students"
+  "SELECT MIN(gpa) as lowest_gpa FROM students;"
 end
 
 def average_student_gpa
-  "SELECT AVG(gpa) FROM students"
+  "SELECT AVG(gpa) FROM students;"
 end
 
 def total_tardies_for_all_students
-  "SELECT SUM(tardies) AS total_tardies FROM students"
+  "SELECT SUM(tardies) AS total_tardies FROM students;"
 end
 
 def average_gpa_for_9th_grade
-  "SELECT AVG(gpa) FROM students WHERE grade "
+  "SELECT AVG(gpa) FROM students WHERE grade==9;"
 end
